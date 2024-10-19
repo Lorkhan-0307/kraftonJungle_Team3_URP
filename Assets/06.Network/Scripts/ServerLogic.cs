be60d6f64dca3d3c0951c627a982d1a6610c4a07
897754 <42639194+897754@users.noreply.github.com>
2024-10-19 23:46:40 +0900

    GameResult Fix

--- Assets/06.Network/Scripts/ServerLogic.cs
+++ Assets/06.Network/Scripts/ServerLogic.cs
@@ -113,7 +113,7 @@ public class ServerLogic : MonoBehaviourPunCallbacks
         for(int i = 0; i < isAlivePlayers.Length; i++ )
         {
             if(isAlivePlayers[i])
-                result.Add(i);
+                result.Add(i+1);
         }
 
         return result;
