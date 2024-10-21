501033aabb2f7af399174152e01a64cf5a1a8b61
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-10-21 23:15:28 +0900

    bugfix on material

--- Assets/05.Scripts/Etc/MonsterCamera.cs
+++ Assets/05.Scripts/Etc/MonsterCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine.Rendering;
 public class MonsterOutlineEffect : MonoBehaviour
 {
     public Material outlineMaterial; // 여러 아웃라인 마테리얼 배열
+    public Material originalMaterial; // 여러 아웃라인 마테리얼 배열
 
     public void EnableOutlineEffect()
     {
@@ -15,17 +16,7 @@ public class MonsterOutlineEffect : MonoBehaviour
 
             if (skinnedMeshRenderer != null)
             {
-                // 기존 머티리얼을 복사하고 새 머티리얼을 추가합니다.
-                Material[] originalMaterials = skinnedMeshRenderer.materials;
-                Material[] newMaterials = new Material[originalMaterials.Length + 1];
-
-                for (int i = 0; i < originalMaterials.Length; i++)
-                {
-                    newMaterials[i] = originalMaterials[i];
-                }
-
-                newMaterials[newMaterials.Length - 1] = outlineMaterial;
-                skinnedMeshRenderer.materials = newMaterials;
+                skinnedMeshRenderer.material = outlineMaterial;
             }
         }
     }
@@ -33,6 +24,8 @@ public class MonsterOutlineEffect : MonoBehaviour
     public void DisableOutlineEffect()
     {
         GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("ScientistOutline");
+
+        if (taggedObjects.Length == 0) return;
     
         foreach (GameObject obj in taggedObjects)
         {
@@ -40,23 +33,7 @@ public class MonsterOutlineEffect : MonoBehaviour
 
             if (skinnedMeshRenderer != null)
             {
-                // 기존 머티리얼 목록에서 새 머티리얼을 제거합니다.
-                Material[] originalMaterials = skinnedMeshRenderer.materials;
-                if (originalMaterials.Length <= 1) continue; // 머티리얼이 하나 이하일 경우 무시
-
-                Material[] newMaterials = new Material[originalMaterials.Length - 1];
-                int index = 0;
-
-                for (int i = 0; i < originalMaterials.Length; i++)
-                {
-                    if (originalMaterials[i] != outlineMaterial)
-                    {
-                        newMaterials[index] = originalMaterials[i];
-                        index++;
-                    }
-                }
-
-                skinnedMeshRenderer.materials = newMaterials;
+                skinnedMeshRenderer.material = originalMaterial;
             }
         }
     }
