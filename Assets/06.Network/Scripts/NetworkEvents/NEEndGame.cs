6272474f6f0b0d41016ca5ab6be697c30a49f793
897754 <42639194+897754@users.noreply.github.com>
2024-10-24 13:23:16 +0900

    Added Game Result Log

--- Assets/06.Network/Scripts/NetworkEvents/NEEndGame.cs
+++ Assets/06.Network/Scripts/NetworkEvents/NEEndGame.cs
@@ -18,13 +18,28 @@ public class NEEndGame : NetworkEvent
         GameManager.instance.EndGame();
 
         object[] customDatas = (object[])customData;
+
         bool isMonsterWon = (bool)customDatas[0];
+        string resultStr = (string)customDatas[1];
+
+        //PlayerPrefs 에 임시로 결과 추가
+        AddResult(isMonsterWon, resultStr);
+
 
         GameOverPopup popup = FindObjectOfType<GameOverPopup>();
         popup.SetupWinner(isMonsterWon);
         popup.GetComponent<ModalWindowManager>().ModalWindowIn();
     }
 
+    void AddResult(bool isMonsterWon, string resultStr)
+    {
+        string[] tokens = resultStr.Split(',');
+
+        string addResult = $"{isMonsterWon.ToString()},{tokens.Length}\n";
+
+        string pref = PlayerPrefs.GetString("GameResultDemo", "");
+        PlayerPrefs.SetString("GameResultDemo", pref + addResult);
+    }
 }
 
 public class GameResultPlayerData
