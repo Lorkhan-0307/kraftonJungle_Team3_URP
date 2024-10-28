65d06993b5ceec22510e024e3a3a5697defe063b
lazyArtisan <biomatrix117@naver.com>
2024-10-28 22:21:33 +0900

    Bacis Spectator Indicating

--- Assets/05.Scripts/Manager/SpectatorManager.cs
+++ Assets/05.Scripts/Manager/SpectatorManager.cs
@@ -4,15 +4,15 @@ using UnityEngine.InputSystem;
 
 public class SpectatorManager : Singleton<SpectatorManager>
 {
-    public GameObject spectatorPrefab; // Reference to your Spectator prefab
+    [SerializeField] private GameObject spectatorPrefab;
     private bool isSpectating = false;
     private List<GameObject> remainingPlayers = new List<GameObject>();
     private GameObject spectatorInstance;
 
-    // private void Awake()
-    // {
-    //     StartSpectating();
-    // }
+    private void Awake()
+    {
+        StartSpectating();
+    }
 
     // 자신이 죽었을 때 호출
     public void StartSpectating()
@@ -22,10 +22,13 @@ public class SpectatorManager : Singleton<SpectatorManager>
         spectatorInstance = Instantiate(spectatorPrefab);
     }
 
+    // TODO : 동시에 죽었을 때 리스트 제대로 업뎃 안되는 문제 해결
     // 플레이어가 죽었을 때 호출
     public void RemoveRemainingPlayer(GameObject player)
     {
         if (isSpectating)
+        {
             spectatorInstance.GetComponent<SpectatorCamera>().RemovePlayer(player);
+        }
     }
 }
