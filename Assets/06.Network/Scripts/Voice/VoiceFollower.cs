17d58b2fb3da366bee672bbbe783cf638130138d
897754 <42639194+897754@users.noreply.github.com>
2024-10-30 23:34:29 +0900

    Update VoiceFollower.cs

--- Assets/06.Network/Scripts/Voice/VoiceFollower.cs
+++ Assets/06.Network/Scripts/Voice/VoiceFollower.cs
@@ -8,7 +8,7 @@ public class VoiceFollower : MonoBehaviour
     {
         get
         {
-            return NetworkManager.Instance.myPlayer.transform;
+            return NetworkManager.Instance?.myPlayer?.transform;
         }
     }
 
