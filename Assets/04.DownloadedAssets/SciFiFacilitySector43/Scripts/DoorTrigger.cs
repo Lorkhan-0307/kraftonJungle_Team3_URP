a23b6be417f1e819e399c69b5ade996c2e238906
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-10-31 15:21:16 +0900

    added new map

---
+++ Assets/04.DownloadedAssets/SciFiFacilitySector43/Scripts/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private Door_Download door;
+
+    void Start()
+    {
+        door = GetComponentInParent<Door_Download>();
+    }
+
+    void OnTriggerEnter(Collider c) {
+
+        door.openDoor(c);
+
+    }
+
+    void OnTriggerExit(Collider c) {
+        door.closeDoor(c);
+    }
+}
