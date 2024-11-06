9d81a1dc00b8df0d989931edb76df38c37626a94
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-11-06 20:33:43 +0900

    update spectator

--- Assets/05.Scripts/Spectator/SpectatorCameraColorChange.cs
+++ Assets/05.Scripts/Spectator/SpectatorCameraColorChange.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 public class SpectatorCameraColorChange : MonoBehaviour
 {
     public Material ScientistBodyMaterial; // 과학자 몸통 머테리얼
-    public Material ScientistBody1Mateiral; // 과학자 손발 머테리얼
     public Material MonsterBodyMaterial; // 몬스터 몸통 머테리얼
-    public Material MonsterBody1Material; // 몬스터 손발 머테리얼
 
     public void EnableOutlineEffect()
     {
@@ -18,10 +16,9 @@ public class SpectatorCameraColorChange : MonoBehaviour
 
             if (skinnedMeshRenderer != null)
             {
-                Material[] materials = skinnedMeshRenderer.materials;
-                materials[1] = ScientistBodyMaterial;
-                materials[0] = ScientistBody1Mateiral;
-                skinnedMeshRenderer.materials = materials;
+                Material[] newMats = skinnedMeshRenderer.materials.Clone() as Material[];
+                newMats[0] = ScientistBodyMaterial;
+                skinnedMeshRenderer.materials = newMats;
             }
         }
 
@@ -33,10 +30,9 @@ public class SpectatorCameraColorChange : MonoBehaviour
 
             if (skinnedMeshRenderer != null)
             {
-                Material[] materials = skinnedMeshRenderer.materials;
-                materials[1] = MonsterBodyMaterial;
-                materials[0] = MonsterBody1Material;
-                skinnedMeshRenderer.materials = materials;
+                Material[] newMats = skinnedMeshRenderer.materials.Clone() as Material[];
+                newMats[0] = MonsterBodyMaterial;
+                skinnedMeshRenderer.materials = newMats;
             }
         }
 
