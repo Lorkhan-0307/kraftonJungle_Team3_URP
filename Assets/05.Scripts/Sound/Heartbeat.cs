e1580d5e70edea3a5dc232dd632c10915b47e91b
lazyArtisan <biomatrix117@naver.com>
2024-11-06 18:15:13 +0900

    delay heartbeat implement

--- Assets/05.Scripts/Sound/Heartbeat.cs
+++ Assets/05.Scripts/Sound/Heartbeat.cs
@@ -1,54 +1,61 @@
-using System.Collections.Generic;
-using UnityEngine;
-using Photon.Pun;
-using System.Linq;
-public class Heartbeat : MonoBehaviour
-{
-    [SerializeField] private AudioSource heartbeatSound;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Photon.Pun;
+// using System.Linq;
+// public class Heartbeat : MonoBehaviour
+// {
+//     [SerializeField] private AudioSource heartbeatSound;
 
-    private GameObject Monster;
+//     private GameObject Monster;
 
 
-    private void Update()
-    {
-        if (!NetworkManager.Instance || NetworkManager.Instance.IsMonster()) return;
+//     private void Update()
+//     {
+//         if (!NetworkManager.Instance || NetworkManager.Instance.IsMonster()) return;
 
-        if (Monster == null)
-        {
-            List<GameObject> Monsters = NetworkManager.Instance.Monsters.Values.Select(monster => monster.gameObject).ToList();
-            if (Monsters.Count > 0)
-            {
-                Monster = Monsters[0];
-            }
-            else
-            {
-                return;
-            }
-        }
+//         if (Monster == null)
+//         {
+//             List<GameObject> Monsters = NetworkManager.Instance.Monsters.Values.Select(monster => monster.gameObject).ToList();
+//             if (Monsters.Count > 0)
+//             {
+//                 Monster = Monsters[0];
+//             }
+//             else
+//             {
+//                 Debug.Log("몬스터가 없음");
+//                 return;
+//             }
+//         }
 
-        // 밤이고, 관전 중이 아닐 때, 몬스터와 가까워질수록 빠르고 크게 심장소리 재생
-        if (!TimeManager.instance.isDay && !SpectatorManager.instance.isSpectating)
-        {
-            float distance = Vector3.Distance(Monster.transform.position, transform.position);
-            if (distance < 10f)
-            {
-                heartbeatSound.volume = 2f;
-                heartbeatSound.pitch = 2f;
-            }
-            else if (distance < 20f)
-            {
-                heartbeatSound.volume = 1f;
-                heartbeatSound.pitch = 1f;
-            }
-            else
-            {
-                heartbeatSound.volume = 0.5f;
-                heartbeatSound.pitch = 0.5f;
-            }
-        }
-        else
-        {
-            heartbeatSound.volume = 0f;
-        }
-    }
-}
+//         if (!TimeManager.instance.isDay)
+//             Debug.Log("낮임");
+
+//         if (!SpectatorManager.instance.isSpectating)
+//             Debug.Log("관전 중이 아님");
+
+//         // 밤이고, 관전 중이 아닐 때, 몬스터와 가까워질수록 빠르고 크게 심장소리 재생
+//         if (!TimeManager.instance.isDay && !SpectatorManager.instance.isSpectating)
+//         {
+//             float distance = Vector3.Distance(Monster.transform.position, transform.position);
+//             if (distance < 10f)
+//             {
+//                 heartbeatSound.volume = 2f;
+//                 heartbeatSound.pitch = 2f;
+//             }
+//             else if (distance < 20f)
+//             {
+//                 heartbeatSound.volume = 1f;
+//                 heartbeatSound.pitch = 1f;
+//             }
+//             else
+//             {
+//                 heartbeatSound.volume = 0.5f;
+//                 heartbeatSound.pitch = 0.5f;
+//             }
+//         }
+//         else
+//         {
+//             heartbeatSound.volume = 0f;
+//         }
+//     }
+// }
