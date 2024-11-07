a340a2d235b3559b5c203e57708b1cec5feee3ac
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-11-07 12:32:12 +0900

    update fixed update

--- Assets/05.Scripts/Character/PlayerMovement.cs
+++ Assets/05.Scripts/Character/PlayerMovement.cs
@@ -155,7 +155,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (VoiceManager)
         {
