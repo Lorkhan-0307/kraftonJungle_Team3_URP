98ec14306e25c29ce90565c691dbe198408c8ec7
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-11-13 18:57:57 +0900

    update for memory

--- Assets/06.Network/Scripts/UI/DisplayPing.cs
+++ Assets/06.Network/Scripts/UI/DisplayPing.cs
@@ -1,3 +1,4 @@
+using System;
 using Photon.Pun;
 using System.Collections;
 using TMPro;
@@ -12,6 +13,9 @@ public class DisplayPing : MonoBehaviour
 
     WaitForSeconds wait;
 
+    private int original_ping = -999;
+    private int new_ping;
+
     private void Start()
     {
         wait = new WaitForSeconds(refreshRate);
@@ -22,9 +26,14 @@ public class DisplayPing : MonoBehaviour
     {
         while (pingText != null)
         {
-            // PhotonNetwork.GetPing()를 통해 현재 Ping 값을 가져와 UI에 표시
-            pingText.text = $"Ping: {PhotonNetwork.GetPing()} ms";
-            yield return wait;
+            new_ping = PhotonNetwork.GetPing();
+            if (new_ping != original_ping)
+            {
+                original_ping = new_ping;
+                pingText.text = $"Ping: {original_ping} ms";
+                yield return wait;
+            }
+                // PhotonNetwork.GetPing()를 통해 현재 Ping 값을 가져와 UI에 표시
         }
     }
 
