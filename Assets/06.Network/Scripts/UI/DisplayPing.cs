1638939efa7272e3c36aad292ed8603a1a5c56b5
SEUNGMIN LEE <39990300+Lorkhan-0307@users.noreply.github.com>
2024-11-13 21:54:44 +0900

    ping revert

--- Assets/06.Network/Scripts/UI/DisplayPing.cs
+++ Assets/06.Network/Scripts/UI/DisplayPing.cs
@@ -11,6 +11,10 @@ public class DisplayPing : MonoBehaviour
     float refreshRate = 1f;
 
     WaitForSeconds wait;
+    
+    private int original_ping = -999;
+    private int new_ping;
+
 
     private void Start()
     {
@@ -18,13 +22,29 @@ public class DisplayPing : MonoBehaviour
         StartCoroutine(RefreshPing());
     }
 
-    IEnumerator RefreshPing()
+    /*IEnumerator RefreshPing()
     {
         while (pingText != null)
         {
             // PhotonNetwork.GetPing()를 통해 현재 Ping 값을 가져와 UI에 표시
             pingText.text = $"Ping: {PhotonNetwork.GetPing()} ms";
             yield return wait;
+        }
+    }*/
+    
+    IEnumerator RefreshPing()
+    {
+        while (pingText != null)
+        {
+            // PhotonNetwork.GetPing()를 통해 현재 Ping 값을 가져와 비교 후 다르면 교체
+            new_ping = PhotonNetwork.GetPing();
+            if (new_ping != original_ping)
+            {
+                original_ping = new_ping;
+                pingText.text = $"Ping: {original_ping} ms";
+            }
+            yield return wait;
+            // PhotonNetwork.GetPing()를 통해 현재 Ping 값을 가져와 UI에 표시
         }
     }
 
